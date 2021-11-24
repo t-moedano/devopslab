52caca5b56ac4fff1cfda50cffa17f2e07810b98
Thauany Moedano <moedano.t@gmail.com>
2021-11-23 23:06:37 -0300

    "Bug Removido"

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
