71c02c8d9af80517b8d6041d219d79fe3dddb61e
Thauany Moedano <thauany@zappts.com>
2022-08-27 12:10:18 -0300

    adicionando dockerfile

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
+
 
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
+
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
