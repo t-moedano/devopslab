17dc0ff86e0a179757938242901e60a5350a9873
Thauany Moedano <thauany@zappts.com>
2022-10-07 22:52:20 -0300

    commit lab devops video

--- test.py
+++ test.py
@@ -18,4 +18,5 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Teste Video 01")
+        self.assertEqual(self.result.data.decode(
+            'utf-8'), "Video para o Lab - fase 05!")
